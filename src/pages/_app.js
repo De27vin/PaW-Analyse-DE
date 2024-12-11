3bd254b6404ab87aca52e9155a064f9d3302a597
mikeezvz <mike.zogheib@outlook.com>
2024-12-11 13:50:56 +0100

    Initial commit after re-setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+    <nav className="flex justify-between bg-gray-900 text-white w-screen">
+      <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+        <Link className="text-3xl font-bold font-heading" href="/">
+          Rechtsrutsch DE
+        </Link>
+
+        <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+          <li><Link className="hover:text-gray-200" href="/">Home</Link></li>
+          <li><Link className="hover:text-gray-200" href="/armut-in-deutschland">Armut in DE</Link></li>
+          <li><Link className="hover:text-gray-200" href="/">Die AfD</Link></li>
+        </ul>
+        <div className="mx-auto">Github</div>
+      </div>
+    </nav>
+  <Component {...pageProps} />;
+  </>
+  )
 }
