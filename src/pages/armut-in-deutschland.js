a7b98bd1ff02cefdc508da498406e0cf62928486
mikeezvz <mike.zogheib@outlook.com>
2024-12-12 13:32:42 +0100

    add: Div with gray color

--- src/pages/armut-in-deutschland.js
+++ src/pages/armut-in-deutschland.js
@@ -3,6 +3,23 @@ import Image from 'next/image'
 export default function ArmutInDeutschlandPage() {
 return (
     <>
+    <div className="grid grid-cols-2 gap-10 h-20 bg-gray-500 m-10 h-auto p-10">
+    <div className='relative'>
+        <br/>
+        <section className='bg-gray-900'>
+        <Image
+        src="/images/poverty.png"
+        width={1000}
+        height={1000}
+        className='object-cover opacity-60'
+        />  
+        </section>
+    </div>
+    <div className='w-3/4'>
+        <p className='text-2xl font-bold'>Lorem Ipsum</p> 
+        <p>Lorem Ipsum</p>
+    </div>
+    </div>
     </>
  );
-}
+} 
