830b4f30c4e4d2b072a3437765157c171b089106
mikeezvz <mike.zogheib@outlook.com>
2024-12-13 14:11:51 +0100

    add: Data and python script
    A python file and two files .csv were added.

--- src/pages/armut-in-deutschland.js
+++ src/pages/armut-in-deutschland.js
@@ -20,6 +20,24 @@ return (
         <p className='text-xl'>Die Armut zeigt sich in Deutschland bis heute noch und das obwohl es eines der wohlhabendsten Länder der Welt ist. Die Gruft zwischen Arm und Reich zeigt sich hier aber eher relativ. Menschen können sich kulturell nicht mehr aktiv beteiligen und müssen sich um ihre finanzielle Zukunft sorgen. Dabei ist auch vor allem Ost-DE betroffen, welches immer noch von den Folgen der Auflösung der DDR betroffen ist. </p>
     </div>
     </div>
+
+    <div className="grid grid-cols-2 gap-10 h-20 bg-gray-600 m-10 h-auto p-10">
+    <div className='w-3/4'>
+        <p className='text-2xl font-bold'>Das BiP pro Kopf</p> 
+        <p className='text-xl'>Die Armut zeigt sich in Deutschland bis heute noch und das obwohl es eines der wohlhabendsten Länder der Welt ist. Die Gruft zwischen Arm und Reich zeigt sich hier aber eher relativ. Menschen können sich kulturell nicht mehr aktiv beteiligen und müssen sich um ihre finanzielle Zukunft sorgen. Dabei ist auch vor allem Ost-DE betroffen, welches immer noch von den Folgen der Auflösung der DDR betroffen ist. </p>
+    </div>
+    <div className='relative'>
+        <br/>
+        <section className='bg-gray-900'>
+        <Image
+        src="/images/poverty.png"
+        width={1000}
+        height={1000}
+        className='object-cover opacity-60'
+        />  
+        </section>
+    </div>
+    </div>
     </>
  );
 } 
