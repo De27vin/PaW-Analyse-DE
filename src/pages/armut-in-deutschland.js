4c85a14b3faa0bf17dc576ce1066ed5392c73b77
mikeezvz <mike.zogheib@outlook.com>
2024-12-18 10:53:24 +0100

    add: more text in armut-in-deutschland.js

--- src/pages/armut-in-deutschland.js
+++ src/pages/armut-in-deutschland.js
@@ -17,14 +17,15 @@ return (
     </div>
     <div className='w-3/4'>
         <p className='text-2xl font-bold'>Die Armut in Deutschland</p> 
-        <p className='text-xl'>Die Armut zeigt sich in Deutschland bis heute noch und das obwohl es eines der wohlhabendsten Länder der Welt ist. Die Gruft zwischen Arm und Reich zeigt sich hier aber eher relativ. Menschen können sich kulturell nicht mehr aktiv beteiligen und müssen sich um ihre finanzielle Zukunft sorgen. Dabei ist auch vor allem Ost-DE betroffen, welches immer noch von den Folgen der Auflösung der DDR betroffen ist. </p>
+        <p className='text-xl'>Die Armut zeigt sich in Deutschland bis heute noch und das obwohl es eines der wohlhabendsten Länder der Welt ist. Die Gruft zwischen Arm und Reich zeigt sich hier aber eher relativ. Menschen können sich kulturell nicht mehr aktiv beteiligen und müssen sich um ihre finanzielle Zukunft sorgen. Dabei ist auch vor allem Ost-DE betroffen, welches immer noch von den Folgen der Auflösung der DDR betroffen ist. Zum Teil liegt es auch an der Arbeitskultur in den jeweiligen Bundesländern. Während Bayern mit seinem BiP alleine schon fast mit der Schweiz mithalten kann, hat z.B. Mecklenburg-Vorpommern eines der tiefsten BiP's und das trotz Anbindung an wichtige Handelsrouten. </p>
     </div>
     </div>
 
     <div className="grid grid-cols-2 gap-10 h-20 bg-gray-600 m-10 h-auto p-10">
     <div className='w-3/4'>
         <p className='text-2xl font-bold'>Das BiP</p> 
-        <p className='text-xl'>Auf dieser Grafik sieht man das BiP und das BiP pro Kopf für jedes Bundesland.Wohlhabende Wirtschaftszentren wie Nordrhein-Westfalen oder Bayern haben eher ein stablies BiP. Ostdeutsche Bundesländer haben eher ein tieferes BiP und das zeigt sich in den jeweiligen Bundesländern auch. </p>
+        <p className='text-xl'>Auf dieser Grafik sieht man das BiP und das BiP pro Kopf für jedes Bundesland. Wohlhabende Wirtschaftszentren wie Nordrhein-Westfalen oder Bayern haben eher ein stablies BiP. Ostdeutsche Bundesländer haben eher ein tieferes BiP und das zeigt sich in den jeweiligen Bundesländern auch. Armut und Entfremdung gegenüber dem Staat sind für einige Menschen in diesen Bundesländern schon Realität. Solche Menschen sind meist auch empfänglicher für extreme Denkweisen und eine rechte politische Ausrichtung. Auch Strukturdefizite und fehlgeschlagene Pläne für die Förderung mit finanziellen Hilfspaketen sind ein Grund warum viele Menschen nicht der Armut entkommen und es zum Teil über Generationen übertragen wird.
+</p>
     </div>
     <div className='relative'>
         <br/>
